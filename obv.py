1013653507c245a85f437785e1aee7d25e619db5
Skullamortis <jv2035@gmail.com>
2021-01-23 16:27:28 -0300

    Iterador ainda pula candles [need FIX]

--- obv.py
+++ obv.py
@@ -2,20 +2,32 @@
 
 import pandas as pd
 import numpy as np
+import time
 
 data = pd.read_csv('candle5m.csv',index_col=0,parse_dates=True)
 obv_init = 0 
 close_init = data['close'].values[0]
-index_next = np.datetime64(data.index[0])
+index_next = pd.Timestamp(data.index[0])
 obv = 0
 iterator = data.iterrows()
+candles = 0
+print (obv)
+
 
 for index, row in iterator:
     
-    close_value = row['close']
-    volume_value = row['volume']
-        
+    try:
+        index_next = next(iterator)
+        index_next = index_next[0]
+    except:
+        print ("End of file")
+        print ("Total candles:", candles)
+
     if index != index_next:
+    
+        close_value = row['close']
+        volume_value = row['volume']
+    
         if close_init > close_value:
             obv = obv_init - volume_value
     
@@ -25,10 +37,14 @@ for index, row in iterator:
         else:
             obv = 0
         
-        print(obv)
+        print ("Candle Date:", index, "OBV:",obv, "Close_init:", close_init, "Close:", close_value, "Volume:", volume_value)
+#        print ("Next date:", index_next)
+        #print (index_next)
+        candles = candles + 1
+        obv_init = obv
+        close_init = close_value 
 
-    obv_init = obv
-    close_init = close_value 
-    index_next = np.datetime64(index)      
+    
+
     
 
