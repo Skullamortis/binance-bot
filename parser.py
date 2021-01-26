1532c8fcdc3e07608fc4574843bcbf1558772071
Skullamortis <jv2035@gmail.com>
2021-01-25 22:29:33 -0300

    Adicionado progress_bar.

--- parser.py
+++ parser.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import json
 import datetime
+from tqdm import tqdm
+import sys
+
+try:
+    arg1, arg2 = sys.argv[1], sys.argv[2]
+except:
+    sys.exit("Usage 'python parser.py [filename.txt] [file_output.csv]")
 
 data = []
-with open('data5m.txt') as f:
+
+with open(arg1) as f:
     for line in f:
         data.append(json.loads(line))
 numbers = len(data)
 df=pd.DataFrame(columns=['date', 'open', 'high', 'low', 'close','volume'])
 
-for i in range(numbers):
+for i in tqdm(range(numbers)):
     date = (data[i]['data']['k']['t'])/1000
     sopen = data[i]['data']['k']['o']
     high =  data[i]['data']['k']['h']
@@ -17,7 +25,7 @@ for i in range(numbers):
     close = data[i]['data']['k']['c']
     volume = data[i]['data']['k']['v']
     df = df.append({"date":date,"open":sopen,"high":high,"low":low,"close":close,"volume":volume},ignore_index=True)
-
+    
 df['date'] = df['date'].apply(lambda x:datetime.datetime.fromtimestamp(x))
-df.to_csv('candle5m.csv', index=False)
-print (df)
+print ("Writting to file...")
+df.to_csv(arg2, index=False)
